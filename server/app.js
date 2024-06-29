fbdda936c87aea16493b13b99657a82ca7f0f500
Pranjalshukla1602 <pranjalshukla800@gmail.com>
2024-06-29 13:05:35 +0530

    done

--- server/app.js
+++ server/app.js
@@ -14,32 +14,11 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// Serve the static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-// Serve the static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
-
-// API routes
-app.get('/api/hello', (req, res) => {
-  res.send({ message: 'Hello from server!' });
-});
-
-// Handles any requests that don't match the ones above
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
-});
-
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
